dbcab5379375d7bfbaeda45a3d8bb10dfd608749
Valentin Kaisermayer <50108075+ValentinKaisermayer@users.noreply.github.com>
2022-04-11 20:39:30 +0200

    Update utils.jl

--- src/Thermal/utils.jl
+++ src/Thermal/utils.jl
@@ -1,4 +1,4 @@
 @connector function HeatPort(; name)
-    @variables T(t)=273.15, Q_flow(t)=0.0 [connect=Flow] # Temperature and Heat-flow-rate
+    @variables T(t)=273.15 Q_flow(t)=0.0 [connect=Flow] # Temperature and Heat-flow-rate
     ODESystem(Equation[], t, [T, Q_flow], [], name=name)
 end
