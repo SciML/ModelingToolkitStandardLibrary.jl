b27b4ff4954a150aa611f1e4260ca0f80125259a
Valentin Kaisermayer <valentin.kaisermayer@best-research.eu>
2022-04-15 19:00:17 +0200

    adds sources

--- src/Blocks/sources.jl
+++ src/Blocks/sources.jl
@@ -21,17 +21,63 @@ function SinSource(;name,
     starttime=0)
 
     @named output = RealOutput()
-    pars = @parameters offset=offset startTime=starttime amplitude=amplitude frequency=frequency phase=phase
+    pars = @parameters offset=offset startime=starttime amplitude=amplitude frequency=frequency phase=phase
     eqs = [
-        output.u ~ offset + ifelse(t < startTime, 0, amplitude* sin(2*pi*frequency*(t - startTime) + phase))
+        output.u ~ offset + ifelse(t < startime, 0, amplitude* sin(2*pi*frequency*(t - startime) + phase))
+    ]
+    compose(ODESystem(eqs, t, [], pars; name=name), [output])
+end
+
+"""
+Generate clock signal.
+"""
+function ClockSource(;name, 
+    offset=0, # Offset of output signal
+    starttime=0)
+
+    @named output = RealOutput()
+    pars = @parameters offset=offset starttime=starttime
+    eqs = [
+        output.u ~ offset + ifelse(t < starttime, 0, t - starttime)
+    ]
+    compose(ODESystem(eqs, t, [], pars; name=name), [output])
+end
+
+"""
+Generate ramp signal.
+"""
+function RampSource(;name, 
+    offset=0, # Offset of output signal
+    height=1,
+    duration, 
+    starttime=0)
+
+    @named output = RealOutput()
+    pars = @parameters offset=offset starttime=starttime height=height duration=duration
+    eqs = [
+        output.u ~ offset + ifelse(t < starttime, 0, 
+            ifelse(t < (starttime + duration), (t - starttime) * height / duration, height))
+    ]
+    compose(ODESystem(eqs, t, [], pars; name=name), [output])
+end
+
+"""
+Generate step signal.
+"""
+function StepSource(;name, 
+    offset=0, # Offset of output signal
+    height=1,
+    starttime=0)
+
+    @named output = RealOutput()
+    pars = @parameters offset=offset starttime=starttime height=height
+    eqs = [
+        output.u ~ offset + ifelse(t < starttime, 0, height)
     ]
     compose(ODESystem(eqs, t, [], pars; name=name), [output])
 end
 
 # TODO:
-# - Clock   Generate actual time signal
-# - Step    Generate step signal of type Real
-# - Ramp    Generate ramp signal
 # - ExpSine Generate exponentially damped sine signal
 # - Exponentials    Generate a rising and falling exponential signal
 # - Pulse   Generate pulse signal of type Real
