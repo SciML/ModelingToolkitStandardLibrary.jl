8c0d87fce94d226eff41dd6dbefacbc4f1d32ae1
Valentin Kaisermayer <valentin.kaisermayer@best-research.eu>
2022-04-22 14:44:42 +0200

    adds tests for Limiter and DeadZone

--- test/Blocks/nonlinear.jl
+++ test/Blocks/nonlinear.jl
@@ -4,48 +4,86 @@ using ModelingToolkitStandardLibrary.Blocks
 @parameters t
 
 @testset "Limiter" begin
-    y_max = 0.8
-    y_min = -0.6
+    @testset "Constant" begin
+        @named c = Constant(; k=1)
+        @named int = Integrator(; k=1)
+        @named sat = Limiter(; y_min=-0.6, y_max=0.8)
+        @named model = ODESystem([
+                connect(c.output, int.input), 
+                connect(int.output, sat.input),
+            ], 
+            t, 
+            systems=[int, c, sat],
+        )
+        sys = structural_simplify(model)
+        prob = ODEProblem(sys, [int.x=>1.0], (0.0, 1.0))
 
-    @named c = Constant(; k=1)
-    @named int = Integrator(; k=1)
-    @named sat = Limiter(; y_min, y_max)
-    @named model = ODESystem([
-            connect(c.output, int.input), 
-            connect(int.output, sat.input),
-        ], 
-        t, 
-        systems=[int, c, sat],
-    )
-    sys = structural_simplify(model)
+        sol = solve(prob, Rodas4())
+        @test sol[int.output.u][end] ≈ 2
+        @test sol[sat.output.u][end] ≈ 0.8
+    end
 
-    prob = ODEProblem(sys, [int.x=>1.0], (0.0, 1.0))
+    @testset "Sine" begin
+        @named source = Sine(; frequency=1/2)
+        @named lim = Limiter(; y_max=0.5, y_min=-0.5)
+        @named int = Integrator(; k=1)
+        @named iosys = ODESystem([
+                connect(source.output, lim.input),
+                connect(lim.output, int.input),
+            ],
+            t,
+            systems=[source, lim, int],
+        )
+        sys = structural_simplify(iosys)
 
-    sol = solve(prob, Rodas4())
-    @test sol[int.output.u][end] ≈ 2
-    @test sol[sat.output.u][end] ≈ 0.8
+        prob = ODEProblem(sys, Pair[], (0.0, 10.0))
+
+        sol = solve(prob, Rodas4())
+        @test all(abs.(sol[lim.output.u]) .<= 0.5)
+        # Plots.plot(sol; vars=[source.output.u, lim.output.u])
+    end
 end
 
 @testset "DeadZone" begin
-    u_max = 1
-    u_min = -2
-    
-    @named c = Constant(; k=1)
-    @named int = Integrator(; k=1)
-    @named dz = DeadZone(; u_min, u_max)
-    @named model = ODESystem([
-            connect(c.output, int.input), 
-            connect(int.output, dz.input),
-        ], 
-        t, 
-        systems=[int, c, dz],
-    )
-    sys = structural_simplify(model)
+    @testset "Constant" begin
+        @named c = Constant(; k=1)
+        @named int = Integrator(; k=1)
+        @named dz = DeadZone(; u_min=-2, u_max=1)
+        @named model = ODESystem([
+                connect(c.output, int.input), 
+                connect(int.output, dz.input),
+            ], 
+            t, 
+            systems=[int, c, dz],
+        )
+        sys = structural_simplify(model)
+        prob = ODEProblem(sys, [int.x=>1.0], (0.0, 1.0))
+        sol = solve(prob, Rodas4())
 
-    prob = ODEProblem(sys, [int.x=>1.0], (0.0, 1.0))
+        @test sol[int.output.u][end] ≈ 2
+    end
 
-    sol = solve(prob, Rodas4())
-    @test sol[int.output.u][end] ≈ 2
+    @testset "Sine" begin
+        @named source = Sine(; amplitude=3, frequency=1/2)
+        @named dz = DeadZone(; u_min=-2, u_max=1)
+        @named int = Integrator(; k=1)
+        @named model = ODESystem([
+                connect(source.output, dz.input), 
+                connect(dz.output, int.input),
+            ], 
+            t, 
+            systems=[int, source, dz],
+        )
+        sys = structural_simplify(model)
+        prob = ODEProblem(sys, [int.x=>1.0], (0.0, 10.0))
+        sol = solve(prob, Rodas4())
+
+        @test all(sol[dz.output.u] .<= 2)
+        @test all(sol[dz.output.u] .>= -1)
+        
+        # Plots.plot(sol; vars=[source.output.u, dz.output.u])
+        # Plots.plot(sol[dz.input.u], sol[dz.output.u])
+    end
 end
 
 @testset "SlewRateLimiter" begin
