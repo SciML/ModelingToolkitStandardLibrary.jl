2cc582bac75eaec586c411da88c249fefd4de3c6
Valentin Kaisermayer <valentin.kaisermayer@best-research.eu>
2022-04-25 16:17:58 +0200

    extracts _clamp and _dead_zone and makes tests more strict

--- test/Blocks/nonlinear.jl
+++ test/Blocks/nonlinear.jl
@@ -1,5 +1,6 @@
 using ModelingToolkit, OrdinaryDiffEq
 using ModelingToolkitStandardLibrary.Blocks
+using ModelingToolkitStandardLibrary.Blocks: _clamp, _dead_zone
 
 @parameters t
 
@@ -24,8 +25,9 @@ using ModelingToolkitStandardLibrary.Blocks
     end
 
     @testset "Sine" begin
+        y_min, y_max = -0.3, 0.5
         @named source = Sine(; frequency=1/2)
-        @named lim = Limiter(; y_max=0.5, y_min=-0.5)
+        @named lim = Limiter(; y_max=y_max, y_min=y_min)
         @named int = Integrator(; k=1)
         @named iosys = ODESystem([
                 connect(source.output, lim.input),
@@ -35,12 +37,16 @@ using ModelingToolkitStandardLibrary.Blocks
             systems=[source, lim, int],
         )
         sys = structural_simplify(iosys)
-
+    
         prob = ODEProblem(sys, Pair[], (0.0, 10.0))
-
+    
         sol = solve(prob, Rodas4())
         @test all(abs.(sol[lim.output.u]) .<= 0.5)
+        @test all(isapprox.(sol[lim.output.u], _clamp.(sol[source.output.u], y_min, y_max), atol=1e-2))
+        
         # Plots.plot(sol; vars=[source.output.u, lim.output.u])
+        # Plots.scatter(sol[source.output.u], sol[lim.output.u])
+        # Plots.scatter!(sol[source.output.u], _clamp.(sol[source.output.u], y_min, y_max))
     end
 end
 
@@ -64,8 +70,9 @@ end
     end
 
     @testset "Sine" begin
+        u_min, u_max = -2, 1
         @named source = Sine(; amplitude=3, frequency=1/2)
-        @named dz = DeadZone(; u_min=-2, u_max=1)
+        @named dz = DeadZone(; u_min=u_min, u_max=u_max)
         @named int = Integrator(; k=1)
         @named model = ODESystem([
                 connect(source.output, dz.input), 
@@ -77,12 +84,14 @@ end
         sys = structural_simplify(model)
         prob = ODEProblem(sys, [int.x=>1.0], (0.0, 10.0))
         sol = solve(prob, Rodas4())
-
+    
         @test all(sol[dz.output.u] .<= 2)
         @test all(sol[dz.output.u] .>= -1)
+        @test all(isapprox.(sol[dz.output.u], _dead_zone.(sol[source.output.u], u_min, u_max), atol=1e-2))
         
         # Plots.plot(sol; vars=[source.output.u, dz.output.u])
-        # Plots.plot(sol[dz.input.u], sol[dz.output.u])
+        # Plots.scatter(sol[source.output.u], sol[dz.output.u])
+        # Plots.scatter!(sol[source.output.u], _dead_zone.(sol[source.output.u], u_min, u_max))
     end
 end
 
