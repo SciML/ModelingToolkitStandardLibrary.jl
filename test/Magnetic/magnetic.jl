569a707450426d7c3ea96599a5f71995a6445358
Venkateshprasad <32921645+ven-k@users.noreply.github.com>
2022-05-16 22:33:56 +0530

    Fix electrical components in magnetic test

--- test/Magnetic/magnetic.jl
+++ test/Magnetic/magnetic.jl
@@ -1,6 +1,7 @@
 using ModelingToolkitStandardLibrary.Magnetic, ModelingToolkit, OrdinaryDiffEq, Test
 
 import ModelingToolkitStandardLibrary.Electrical
+import ModelingToolkitStandardLibrary.Blocks
 import ModelingToolkitStandardLibrary.Magnetic
 using ModelingToolkit, OrdinaryDiffEq, Test
 # using Plots
@@ -14,7 +15,8 @@ using ModelingToolkit, OrdinaryDiffEq, Test
     l_Fe = 4*0.065 
     a = b = 0.25
 
-    @named source = Electrical.SineVoltage(amplitude=230*sqrt(2), frequency=50, phase=pi/2)
+    @named source = Blocks.Sine(amplitude=230*sqrt(2), frequency=50, phase=pi/2)
+    @named voltage = Electrical.Voltage()
     @named r = Electrical.Resistor(R=7.5)
     @named ground = Electrical.Ground()
     @named coil = Magnetic.FluxTubes.ElectroMagneticConverter(N=600)
@@ -23,18 +25,19 @@ using ModelingToolkit, OrdinaryDiffEq, Test
     @named r_mFe = Magnetic.FluxTubes.ConstantReluctance(R_m=a * b * l_Fe * mu_Fe)
     @named r_mLeak = Magnetic.FluxTubes.ConstantReluctance(R_m=1.2e6)
     connections = [
-        connect(source.p, r.p)
+        connect(source.output, voltage.V)
+        connect(voltage.p, r.p)
         connect(r.n, coil.p)
-        connect(source.n, coil.n)
+        connect(voltage.n, coil.n)
         connect(coil.port_p, r_mLeak.port_p)
         connect(r_mLeak.port_p, r_mAirPar.port_p)
         connect(r_mAirPar.port_n, r_mFe.port_p)
         connect(r_mFe.port_n, r_mLeak.port_n)
         connect(r_mFe.port_n, coil.port_n)
-        connect(ground.g, source.n)
+        connect(ground.g, voltage.n)
         connect(ground_m.port, r_mFe.port_n)
     ]
-    @named model = ODESystem(connections, t, systems=[source, r, ground, coil, ground_m, r_mAirPar, r_mFe, r_mLeak])
+    @named model = ODESystem(connections, t, systems=[source, r, ground, coil, ground_m, r_mAirPar, r_mFe, r_mLeak, voltage])
     sys = structural_simplify(model)
     prob = ODEProblem(sys, Pair[], (0, 0.1))
     sol = solve(prob, Rodas4())
