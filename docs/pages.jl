9e75a6ce36a71a5bf5859dd21ca1f650c8f09553
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-06-03 08:57:46 -0400

    Create pages.jl

---
+++ docs/pages.jl
@@ -0,0 +1,16 @@
+pages = [
+        "ModelingToolkitStandardLibrary.jl: A Standard Library for ModelingToolkit" => "index.md",
+
+        "Tutorials" => [
+            "RC Circuit" => "tutorials/rc_circuit.md",
+            "Custom Components" => "tutorials/custom_component.md",
+        ],
+
+        "API" => [
+            "Basic Blocks" => "API/blocks.md",
+            "Electrical Components" => "API/electrical.md",
+            "Magnetic Components" => "API/magnetic.md",
+            "Mechanical Components" => "API/mechanical.md",
+            "Thermal Components" => "API/thermal.md"
+        ],
+    ]
