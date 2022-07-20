b70225018f44822a49546f639b66770d2a78517b
kianmolani <molanikian@gmail.com>
2022-07-20 10:26:27 -0400

    Reformat code and add function compute_unit_normal_vector

--- src/Mechanical/MultiBody/Parts/RigidBodies/Box.jl
+++ src/Mechanical/MultiBody/Parts/RigidBodies/Box.jl
@@ -1,42 +1,45 @@
 """
-2D rigid body with rectangular shape. The parameters `s_start` and `s_end` are two points in 2D space. Together with the parameter `width`, the vector drawn between these two points defines the spatial extents of our rigid body. This "two-point" vector implementation differs from Modelica's implementation of the same component. 
-In the Modelica implementation, one point is used to define the position of the origin of a new frame of reference, and a second point is used to create a vector from the origin of this new frame.
+2D rigid body with rectangular shape. The parameters `P` and `Q` are two points in 2D space. Together with the parameter `width`, the vector drawn between points `P` and `Q` defines the spatial extents of our rigid body. This "two-point" vector implementation differs from Modelica's implementation of the same component. In the Modelica 
+implementation, one point is used to define the position of the origin of a new frame of reference, and a second point is used to create a vector from the origin of this new frame.
 
 # Parameters:
-- `s_start`: [m] Point in 2D space that defines the starting position of our rigid body
-- `s_end`: [m] Point in 2D space that defines the ending position of our rigid body
-- `w_start`: [rad/s] Initial angular velocity of the ends of our rigid body, to be defined in a direction perpendicular to the vector that forms the body itself
-- `a_start`: [rad/s^2] Initial angular acceleration of the ends of our rigid body, to be defined in a direction perpendicular to the vector that forms the body itself
+- `P`: [m] Point in 2D space defining one end of our rigid body
+- `Q`: [m] Point in 2D space defining opposite end of our rigid body
+- `ω`: [rad/s] Initial angular velocity of our rigid body. `ω` is to be defined as a vector, placed at opposite ends of our rigid body. This forms a pair of vectors that are anti-parallel
+- `α`: [rad/s²] Initial angular acceleration of our rigid body. `α` is to be defined as a vector, placed at opposite ends of our rigid body. This forms a pair of vectors that are anti-parallel
 - `width`: [m] Width of our rigid body
-- `ρ`: [kg/m^3] Material density of our rigid body
-
+- `ρ`: [kg/m³] Material density of our rigid body
 """
+using LinearAlgebra, SymPy
+
 struct Box
-    s_start::Vector{Float64} = [0.0, 0.0]
-    s_end::Vector{Float64} = [1.0, 1.0]
-    w_start::Float64 = 0.0
-    a_start::Float64 = 0.0
+    P::Vector{Float64} = [0.0, 0.0]
+    Q::Vector{Float64} = [1.0, 1.0]
+    ω::Float64 = 0.0
+    α::Float64 = 0.0
     width::Float64 = 1.0
     ρ::Float64 = 2710.0
 
     @parameters t ρ = ρ
-    @parameters length = sqrt ( ( s_start[1] - s_end[1] ) ^ 2 + ( s_start[2] - s_end[2] ) ^ 2 )
+    @parameters length = norm(P-Q)
+    @parameters COM = [P[1] + Q[1] / 2, P[2] + Q[2] / 2]
     @parameters mass = ρ * length * width
 
     D = Differential(t)
 
     sts = @variables begin
-        x(t) = ( s_start[1] + s_end[1] ) / 2
-        y(t) = ( s_start[2] + s_end[2] ) / 2
-        w(t) = w_start
-        a(t) = a_start
+        P(t) = P
+        Q(t) = Q
+        ω(t) = ω
+        α(t) = α
     end
-
-    eqs = [
-        D(s) ~ w
-        D(w) ~ a
-        m * a ~ 0
-    ]
 end
 
-# ODESystem(eqs, t, sts, [m]; name=name)
+@inline function compute_unit_normal_vector(P::Vector{Float64}, Q::Vector{Float64})
+    Q = Q - P
+    y = symbols("y")
+    R = [1.0, y]
+    y = solveset(Eq(dot(Q,R), 0), y)
+    R[2] = y.args[1]
+    return normalize([R[1], R[2]])
+end
