a662ca94d3e8a00ae5dfc43c55f85934ecfad72b
Brad Carman <brad_carman@instron.com>
2022-09-29 16:15:27 -0400

    fixed test

--- test/Mechanical/translational.jl
+++ test/Mechanical/translational.jl
@@ -1,25 +1,51 @@
-using ModelingToolkitStandardLibrary.Mechanical.Translational, ModelingToolkit,
-      OrdinaryDiffEq,
-      Test
+using ModelingToolkit, OrdinaryDiffEq, Test
+
+import ModelingToolkitStandardLibrary.Mechanical.Translational as TV
+import ModelingToolkitStandardLibrary.Mechanical.TranslationalPosition as TP
 
 @parameters t
 D = Differential(t)
 
-@testset "one falling body" begin
-    #body starts at 1m, moving up at 2m/s in gravity of 10
-    @named body = Body(x0 = 1.0, v0 = 2.0, g = -10.0)
+@testset "spring damper mass fixed" begin
 
-    @named model = ODESystem(body.port.f ~ 0, t, [], []; systems = [body])
+    @named dv = TV.Damper(d=1, v1₀=1)
+    @named dp = TP.Damper(d=1, v1₀=1, s1₀=3, s2₀=1)
 
-    sys = structural_simplify(model)
-    prob = ODEProblem(sys, [], (0, 1.0))
-    sol = solve(prob, ImplicitMidpoint(), dt = 0.1)
+    @named sv = TV.Spring(k=1, v1₀=1, Δs₀=1)
+    @named sp = TP.Spring(k=1, s1₀=3, s2₀=1, l=1)
 
-    #x = g*t^2/2 + v_int*t + x_int
-    #dx = g*t + v_int
+    @named bv = TV.Mass(m=1, v₀=1)
+    @named bp = TP.Mass(m=1, v₀=1, s₀=3)
 
-    @test sol.retcode == :Success
-    @test sol[body.x, 1] == 1.0
-    @test sol[body.dx, 1] == 2.0
-    @test sol[body.x, end] ≈ -10 / 2 + 2.0 + 1.0
+    @named gv =  TV.Fixed()
+    @named gp =  TP.Fixed(s₀=1)
+
+    function simplify_and_solve(damping, spring, body, ground)
+
+        eqs = [connect(spring.T1, body.T, damping.T1)
+               connect(spring.T2, damping.T2, ground.T)
+               ]
+ 
+        @named model = ODESystem(eqs, t; systems = [ground, body, spring, damping])
+ 
+        sys = structural_simplify(model)
+ 
+        println.(full_equations(sys))
+ 
+        prob = ODEProblem(sys, [], (0, 20.0), [])
+        sol = solve(prob, ImplicitMidpoint(), dt=0.01)
+ 
+        return sol
+    end
+
+    solv=simplify_and_solve(dv, sv, bv, gv);
+    solp=simplify_and_solve(dp, sp, bp, gp);
+
+    @test solv[bv.v][1] == 1.0
+    @test solv[bv.v][end] ≈ 0.0 atol = 1e-4
+
+    @test solp[bp.v][1] == 1.0
+    @test solp[bp.v][end] ≈ 0.0 atol = 1e-4
+
+
 end
