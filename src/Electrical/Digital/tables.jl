28ff6e38583320e165b4d2e2ab31b20f97053edd
Venkateshprasad <32921645+ven-k@users.noreply.github.com>
2022-12-30 21:51:45 +0530

    refactor: `_and`, `_not`, `_or`, and `_xor`

--- src/Electrical/Digital/tables.jl
+++ src/Electrical/Digital/tables.jl
@@ -1,59 +1,97 @@
 # AND gate
-AndTable = [
-            # U X F0 F1 Z W L H DC
-            U U F0 U U U F0 U U        # U
-            U X F0 X X X F0 X X        # X
-            F0 F0 F0 F0 F0 F0 F0 F0 F0 # F0
-            U X F0 F1 X X F0 F1 X      # F1
-            U X F0 X X X F0 X X        # Z
-            U X F0 X X X F0 X X        # W
-            F0 F0 F0 F0 F0 F0 F0 F0 F0 # L
-            U X F0 F1 X X F0 F1 X      # H
-            U X F0 X X X F0 X X]       # DC
+const AndTable = [
+                  # U X F0 F1 Z W L H DC
+                  U U F0 U U U F0 U U        # U
+                  U X F0 X X X F0 X X        # X
+                  F0 F0 F0 F0 F0 F0 F0 F0 F0 # F0
+                  U X F0 F1 X X F0 F1 X      # F1
+                  U X F0 X X X F0 X X        # Z
+                  U X F0 X X X F0 X X        # W
+                  F0 F0 F0 F0 F0 F0 F0 F0 F0 # L
+                  U X F0 F1 X X F0 F1 X      # H
+                  U X F0 X X X F0 X X]       # DC
+
+function _and2(a::Logic, b::Logic)
+    AndTable[a.level, b.level]
+end
+_and2(a::Number, b::Logic) = _and2(convert(Logic, a), b)
+_and2(a::Logic, b::Number) = _and2(a, convert(Logic, b))
+_and2(a::Number, b::Number) = _and2(convert(Logic, a), convert(Logic, b))
+
+function _and(x...)
+    y = x[1]
+    for i in 2:lastindex(x)
+        y = _and2(y, x[i])
+    end
+    return y
+end
+
+@register_symbolic _and(a, b)
 
 # NOT gate
-NotTable = [U, X, F1, F0, X, X, F1, F0, X]
+const NotTable = [U, X, F1, F0, X, X, F1, F0, X]
+
+_not(x::Logic) = NotTable[get_logic_level(x)]
+_not(x::Number) = _not(convert(Logic, x))
+
+@register_symbolic _not(x)
 
 # OR gate
-OrTable = [
-           # U X F0 F1 Z W L H DC
-           U U U F1 U U U F1 U        # U
-           U X X F1 X X X F1 X        # X
-           U X F0 F1 X X F0 F1 X      # F0
-           F1 F1 F1 F1 F1 F1 F1 F1 F1 # F1
-           U X X F1 X X X F1 X        # Z
-           U X X F1 X X X F1 X        # W
-           U X F0 F1 X X F0 F1 X      # L
-           F1 F1 F1 F1 F1 F1 F1 F1 F1 # H
-           U X X F1 X X X F1 X]       # DC
+const OrTable = [
+                 # U X F0 F1 Z W L H DC
+                 U U U F1 U U U F1 U        # U
+                 U X X F1 X X X F1 X        # X
+                 U X F0 F1 X X F0 F1 X      # F0
+                 F1 F1 F1 F1 F1 F1 F1 F1 F1 # F1
+                 U X X F1 X X X F1 X        # Z
+                 U X X F1 X X X F1 X        # W
+                 U X F0 F1 X X F0 F1 X      # L
+                 F1 F1 F1 F1 F1 F1 F1 F1 F1 # H
+                 U X X F1 X X X F1 X]       # DC
+
+function _or2(a::Logic, b::Logic)
+    OrTable[a.level, b.level]
+end
+_or2(a::Number, b::Logic) = _or2(convert(Logic, a), b)
+_or2(a::Logic, b::Number) = _or2(a, convert(Logic, b))
+_or2(a::Number, b::Number) = _or2(convert(Logic, a), convert(Logic, b))
+
+function _or(x...)
+    y = x[1]
+    for i in 2:lastindex(x)
+        y = _or2(y, x[i])
+    end
+    return y
+end
+
+@register_symbolic _or(a, b)
 
 # XOR gate
-XorTable = [
-            # U X F0 F1 Z W L H DC
-            U U U U U U U U U     # U
-            U X X X X X X X X     # X
-            U X F0 F1 X X F0 F1 X # F0
-            U X F1 F0 X X F1 F0 X # F1
-            U X X X X X X X X     # Z
-            U X X X X X X X X     # W
-            U X F0 F1 X X F0 F1 X # L
-            U X F1 F0 X X F1 F0 X # H
-            U X X X X X X X X]    # DC
+const XorTable = [
+                  # U X F0 F1 Z W L H DC
+                  U U U U U U U U U     # U
+                  U X X X X X X X X     # X
+                  U X F0 F1 X X F0 F1 X # F0
+                  U X F1 F0 X X F1 F0 X # F1
+                  U X X X X X X X X     # Z
+                  U X X X X X X X X     # W
+                  U X F0 F1 X X F0 F1 X # L
+                  U X F1 F0 X X F1 F0 X # H
+                  U X X X X X X X X]    # DC
 
-function _xor2(a, b)
-    XorTable[logic_level_dict[a], logic_level_dict[b]]
+function _xor2(a::Logic, b::Logic)
+    XorTable[a.level, b.level]
 end
+_xor2(a::Number, b::Logic) = _xor2(convert(Logic, a), b)
+_xor2(a::Logic, b::Number) = _xor2(a, convert(Logic, b))
+_xor2(a::Number, b::Number) = _xor2(convert(Logic, a), convert(Logic, b))
 
 function _xor(x...)
-    y = [x[1]]
+    y = x[1]
     for i in 2:lastindex(x)
-        push!(y, _xor2(x[i], y[i - 1]))
+        y = _xor2(y, x[i])
     end
-    return y[end]
+    return y
 end
 
-@register_symbolic _xor(x...)
 @register_symbolic _xor(a, b)
-@register_symbolic _xor(a, b, c)
-@register_symbolic _xor(a, b, c, d)
-@register_symbolic _xor(a, b, c, d, e)
