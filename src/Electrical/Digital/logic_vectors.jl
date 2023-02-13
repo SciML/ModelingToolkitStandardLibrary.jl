416066875c41a4ecfcf6eb16de29a7c27a679522
Venkateshprasad <32921645+ven-k@users.noreply.github.com>
2023-02-14 00:21:33 +0530

    refactor: reove `level` from logic-vectors

--- src/Electrical/Digital/logic_vectors.jl
+++ src/Electrical/Digital/logic_vectors.jl
@@ -4,50 +4,36 @@ const LogicOrNumber = Union{Logic, Number}
 
 struct StdULogicVector{N} <: AbstractArray{Logic, N}
     logic::Array{Logic, N}
-    level::Array{Int, N}
     function StdULogicVector(l::Array)
-        N = ndims(l)
-        l = Array{Logic}(convert.(Logic, l))
-        new{N}(l, get_logic_level.(l))
+        new{ndims(l)}(Array{Logic}(convert.(Logic, l)))
     end
 end
 
 struct StdLogicVector{N} <: AbstractArray{Logic, N}
     logic::Array{Logic, N}
-    level::Array{Int, N}
     function StdLogicVector(l::Array)
-        N = ndims(l)
-        l = Array{Logic}(convert.(Logic, l))
-        new{N}(l, get_logic_level.(l))
+        new{ndims(l)}(Array{Logic}(convert.(Logic, l)))
     end
 end
 
 const LogicVector = Union{StdULogicVector, StdLogicVector}
 
-Base.size(l::LogicVector) = size(l.logic)
+size(l::LogicVector) = size(l.logic)
 
-Base.axes(l::LogicVector) = axes(l.logic)
+axes(l::LogicVector) = axes(l.logic)
 
-function Base.getindex(s::LogicVector,
-                       i::Int)
-    getindex(s.logic, i)
-end
+getindex(s::LogicVector, i::Int) = getindex(s.logic, i)
 function Base.getindex(s::LogicVector, i1::Int, i2::Int,
                        I::Int...)
     getindex(s.logic, i1, i2, I...)
 end
 
-function Base.setindex!(A::LogicVector, x::Logic, i1::Int)
-    setindex!(A.logic, x, i1)
-    setindex!(A.level, get_logic_level(x), i1)
-end
-
+setindex!(A::LogicVector, x::Logic, i1::Int) = setindex!(A.logic, x, i1)
 function Base.setindex!(A::LogicVector, x::Logic, i1::Int, i2::Int, I::Int...)
     setindex!(A.logic, x, i1, i2, I...)
-    setindex!(A.level, get_logic_level(x), i1, i2, I...)
 end
 
-get_logic_level(s::LogicVector) = s.level
+get_logic_level(s::LogicVector) = Int.(s.logic)
 
 # predefined vectors
 const std_ulogic = StdULogicVector([U, X, F0, F1, Z, W, L, H, DC])
