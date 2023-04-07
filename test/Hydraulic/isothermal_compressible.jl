497b721a91dd82263295f7dfe605f19c3a1e3dac
Brad Carman <brad_carman@instron.com>
2023-04-07 15:10:47 -0400

    fixed tests

--- test/Hydraulic/isothermal_compressible.jl
+++ test/Hydraulic/isothermal_compressible.jl
@@ -6,6 +6,8 @@ import ModelingToolkitStandardLibrary.Mechanical.Translational as T
 @parameters t
 D = Differential(t)
 
+# ------------------------------------------------------------
+# Test fluid domain and Pipe
 function System(N; bulk_modulus, name)
     pars = @parameters begin bulk_modulus = bulk_modulus end
 
@@ -53,46 +55,8 @@ s5_1 = complete(sys5_1)
 # N=5 pipe is compressible, will pressurize more slowly
 @test sols[2][s1_1.vol.port.p][end] > sols[3][s5_1.vol.port.p][end]
 
-function System(; bulk_modulus, name)
-    pars = @parameters begin bulk_modulus = bulk_modulus end
 
-    systems = @named begin
-        fluid = IC.HydraulicFluid(; bulk_modulus)
-        stp = B.Step(; height = 10e5, offset = 0, start_time = 0.05, duration = Inf,
-                     smooth = 0.001)
-        src = IC.InputSource(; p_int = 0)
-        vol1 = IC.DynamicVolume(; p_int = 0, area = 0.01, direction = +1)
-        vol2 = IC.DynamicVolume(; p_int = 0, area = 0.01, direction = -1, x_int = 1.0)
-        mass = T.Mass(; m = 1000, s_0 = 0)
-        res = IC.PipeBase(; p_int = 0, area = 0.001, length = 50.0)
-        cap = IC.Cap(; p_int = 0)
-    end
-
-    eqs = [connect(stp.output, src.input)
-           connect(fluid, src.port, cap.port)
-           connect(src.port, res.port_a)
-           connect(res.port_b, vol1.port)
-           connect(vol1.flange, mass.flange, vol2.flange)
-           connect(vol2.port, cap.port)]
-
-    ODESystem(eqs, t, [], pars; name, systems)
-end
-
-@named sys1 = System(; bulk_modulus = 1e9)
-@named sys2 = System(; bulk_modulus = 2e9)
-
-syss = structural_simplify.([sys1, sys2])
-probs = [ODEProblem(sys, [], (0, 0.2)) for sys in syss];
-dt = 1e-4
-sols = [solve(prob, ImplicitEuler(nlsolve = NEWTON); adaptive = false, dt,
-              initializealg = NoInit()) for prob in probs];
-
-s1 = complete(sys1)
-s2 = complete(sys2)
-
-# less stiff will compress more
-@test maximum(sols[1][s1.mass.s]) > maximum(sols[2][s2.mass.s])
-
+# ------------------------------------------------------------
 # Test the Valve 
 function System(; name)
     pars = []
@@ -124,40 +88,43 @@ sol = solve(prob, ImplicitEuler(nlsolve = NEWTON); adaptive = false, dt,
 # the volume should discharge to 10bar
 @test sol[s.vol.port.p][end] ≈ 10e5
 
-# Test minimum_volume
+# ------------------------------------------------------------
+# Test DynmaicVolume and minimum_volume feature
 function System(; name)
     pars = []
 
     # DynamicVolume values
     area = 0.01
     length = 0.1
-    stop = 0.05
+    
 
     systems = @named begin
         fluid = IC.HydraulicFluid()
-        src = IC.Source(; p = 20e5)
-        snk = IC.Source(; p = 10e5)
+        src1 = IC.InputSource(; p_int = 10e5)
+        src2 = IC.InputSource(; p_int = 10e5)
 
-        vol1 = IC.DynamicVolume(; p_int = 10e5, area, direction = +1)
-        vol2 = IC.DynamicVolume(; p_int = 10e5, area, direction = -1, x_int = length,
-                                minimum_volume = area * stop)
+        vol1 = IC.DynamicVolume(; p_int = 10e5, area, direction = +1, dead_volume=2e-4, minimum_volume=2e-4)
+        vol2 = IC.DynamicVolume(; p_int = 10e5, area, direction = -1, dead_volume=2e-4, minimum_volume=2e-4, x_int = length)
+                                
 
-        mass = T.Mass(; m = 1000)
+        mass = T.Mass(; m = 100)
 
-        res = IC.PipeBase(; p_int = 10e5, area = 0.001, length = 50.0)
+        sin1 = B.Sine(; frequency=0.5, amplitude = +1e5, offset=10e5)
+        sin2 = B.Sine(; frequency=0.5, amplitude = -1e5, offset=10e5)
 
-        valve = IC.Valve(; p_a_int = 20e5, p_b_int = 10e5, area_int = 0, Cd = 1e9)
-        ramp = B.Ramp(; height = 1, duration = 0.1, offset = 0, start_time = 0.001,
-                      smooth = true)
     end
 
-    eqs = [connect(fluid, src.port, snk.port)
-           connect(src.port, valve.port_a)
-           connect(valve.port_b, vol1.port)
+    eqs = [connect(fluid, src1.port, src2.port)
+
+           connect(src1.port, vol1.port)
+
+           connect(src2.port, vol2.port)
+
            connect(vol1.flange, mass.flange, vol2.flange)
-           connect(vol2.port, res.port_a)
-           connect(res.port_b, snk.port)
-           connect(ramp.output, valve.input)]
+           
+           connect(src1.input, sin1.output)
+           connect(src2.input, sin2.output)
+           ]
 
     ODESystem(eqs, t, [], pars; name, systems)
 end
@@ -165,24 +132,28 @@ end
 @named min_vol_system = System()
 s = complete(min_vol_system)
 sys = structural_simplify(min_vol_system)
-prob = ODEProblem(sys, [], (0, 0.8))
+prob = ODEProblem(sys, [], (0, 5.))
+
 sol = solve(prob, ImplicitEuler(nlsolve = NEWTON); adaptive = false, dt,
             initializealg = NoInit())
-sol = solve(prob, ImplicitEuler(nlsolve = NEWTON); dtmin = 1e-4, force_dtmin = true,
-            initializealg = NoInit());
 
-fig = Figure()
-ax = Axis(fig[1, 1]; yscale = log10)
-lines!(ax, sol.t, [NaN; diff(sol.t)])
-fig
+# begin
+#     fig = Figure()
 
-# volume/mass should stop moving at 0.05m
+#     ax = Axis(fig[1,1], ylabel="position [m]", xlabel="time [s]")
+#     lines!(ax, sol.t, sol[s.vol1.x]; label="vol1")
+#     lines!(ax, sol.t, sol[s.vol2.x]; label="vol2")
+    
+#     ax = Axis(fig[2,1], ylabel="pressure [bar]", xlabel="time [s]")
+#     lines!(ax, sol.t, sol[s.vol1.port.p]/1e5; label="vol1")
+#     lines!(ax, sol.t, sol[s.vol2.port.p]/1e5; label="vol2")
+#     Legend(fig[2,2], ax)
+#     fig    
+# end
+
+
+# volume/mass should stop moving at opposite ends
 @test round(sol[s.vol1.x][1]; digits = 2) == 0.0
 @test round(sol[s.vol2.x][1]; digits = 2) == 0.1
-@test round(sol[s.vol1.x][end]; digits = 2) == 0.05
-@test round(sol[s.vol2.x][end]; digits = 2) == 0.05
-
-@test sol[s.vol1.p][1] / 1e5≈10 atol=0.1
-@test sol[s.vol2.p][1] / 1e5≈10 atol=0.1
-@test sol[s.vol1.p][end] / 1e5≈20 atol=0.1
-@test sol[s.vol2.p][end] / 1e5≈20 atol=0.1
+@test round(sol[s.vol1.x][end]; digits = 2) == 0.1
+@test round(sol[s.vol2.x][end]; digits = 2) == 0.0
