e7d5ecce253011825d319f39f7dacc7b52799fce
Brad Carman <brad_carman@instron.com>
2023-05-18 10:22:55 -0400

    format

--- src/Hydraulic/IsothermalCompressible/utils.jl
+++ src/Hydraulic/IsothermalCompressible/utils.jl
@@ -92,10 +92,10 @@ Reference: Introduction to Fluid Mechanics, Fox & McDonald, 5th Edition, equatio
 """
 function friction_factor(dm, area, d_h, density, viscosity, shape_factor)
     u = abs(dm) / (density * area)
-    
+
     Re = density * u * d_h / viscosity
     f_laminar = shape_factor / Re
-    
+
     Re = maximum([Re, 1])
     f_turbulent = (shape_factor / 64) * (0.79 * log(Re) - 1.64)^(-2)
 
