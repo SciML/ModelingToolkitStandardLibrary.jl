20a3ee29adfa82d308412054ffa4b2e576492f24
Yingbo Ma <mayingbo5@gmail.com>
2023-05-24 16:24:16 -0400

    More robust tests

--- test/Hydraulic/isothermal_compressible.jl
+++ test/Hydraulic/isothermal_compressible.jl
@@ -36,7 +36,8 @@ NEWTON = NLNewton(check_div = false, always_new = true, max_iter = 10, relax = 9
     @named sys5_1 = System(5; bulk_modulus = 1e9)
 
     syss = structural_simplify.([sys1_2, sys1_1, sys5_1])
-    probs = [ODEProblem(sys, [], (0, 0.2)) for sys in syss] #ModelingToolkit.missing_variable_defaults(sys)
+    probs = [ODEProblem(sys, ModelingToolkit.missing_variable_defaults(sys), (0, 0.2))
+             for sys in syss]
     sols = [solve(prob, ImplicitEuler(nlsolve = NEWTON); initializealg = NoInit(),
                   dt = 1e-4, adaptive = false)
             for prob in probs]
