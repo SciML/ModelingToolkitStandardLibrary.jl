b57b81b81e001a091711e70d56c38e8af51a015e
Venkateshprasad <32921645+ven-k@users.noreply.github.com>
2023-07-03 17:08:10 +0530

    refactor: refactor Magnetic/FluxTubes components with `@mtkmodel`

--- src/Magnetic/FluxTubes/sources.jl
+++ src/Magnetic/FluxTubes/sources.jl
@@ -1,35 +1,53 @@
 """
+    ConstantMagneticPotentialDifference(; name, V_m = 0.0)
+
 Constant magnetomotive force.
 
 Parameters:
 
   - `V_m`: [A] Magnetic potential difference
 """
-@component function ConstantMagneticPotentialDifference(; name, V_m = 1.0)
-    port_p = PositiveMagneticPort()
-    port_n = NegativeMagneticPort()
-    @parameters V_m = V_m
-    @variables Phi(t)
-    eqs = [V_m ~ port_p.V_m - port_n.V_m
+@mtkmodel ConstantMagneticPotentialDifference begin
+    @components begin
+        port_p = PositiveMagneticPort()
+        port_n = NegativeMagneticPort()
+    end
+    @parameters begin
+        V_m = 0.0, [description = "Magnetic potential difference"]
+    end
+    @variables begin
+        Phi(t)
+    end
+    @equations begin
+        V_m ~ port_p.V_m - port_n.V_m
         Phi ~ port_p.Phi
-        0 ~ port_p.Phi + port_n.Phi]
-    ODESystem(eqs, t, [Phi], [V_m], systems = [port_p, port_n], name = name)
+        0 ~ port_p.Phi + port_n.Phi
+    end
 end
 
 """
+    ConstantMagneticFlux(; name, Phi = 0.0)
+
 Source of constant magnetic flux.
 
 Parameters:
 
   - `Phi`: [Wb] Magnetic flux
 """
-@component function ConstantMagneticFlux(; name, Phi = 1.0)
-    port_p = PositiveMagneticPort()
-    port_n = NegativeMagneticPort()
-    @parameters Phi = Phi
-    @variables V_m(t)
-    eqs = [V_m ~ port_p.V_m - port_n.V_m
+@mtkmodel ConstantMagneticFlux begin
+    @components begin
+        port_p = PositiveMagneticPort()
+        port_n = NegativeMagneticPort()
+    end
+    @parameters begin
+        Phi = 0.0, [description = "Magnetic flux"]
+    end
+    @variables begin
+        V_m(t)
+    end
+    @equations begin
+        V_m ~ port_p.V_m - port_n.V_m
         Phi ~ port_p.Phi
-        0 ~ port_p.Phi + port_n.Phi]
-    ODESystem(eqs, t, [Phi], [V_m], systems = [port_p, port_n], name = name)
+        0 ~ port_p.Phi + port_n.Phi
+    end
 end
