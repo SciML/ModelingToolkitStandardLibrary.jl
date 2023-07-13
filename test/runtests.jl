700c78136412ba8124b8b4497a1eb91b66aa24b5
Venkateshprasad <32921645+ven-k@users.noreply.github.com>
2023-07-13 13:28:42 +0530

    refactor&test: Mechanical/TranslationalModelica with `@mtkmodel`
    - And enable its tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -51,6 +51,9 @@ end
 @safetestset "Mechanical Translation" begin
     include("Mechanical/translational.jl")
 end
+@safetestset "Mechanical Translation" begin
+    include("Mechanical/translational_modelica.jl")
+end
 @safetestset "Multi-Domain" begin
     include("multi_domain.jl")
 end
