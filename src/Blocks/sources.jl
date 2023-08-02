f24200a1cc48ae455d35c2328f9bd617ffcf5e54
Brad Carman <brad_carman@instron.com>
2023-08-02 06:09:21 -0400

    fixed SampleData allocation

--- src/Blocks/sources.jl
+++ src/Blocks/sources.jl
@@ -497,18 +497,28 @@ Base.:+(x::Number, y::Parameter) = x + y.ref
 Base.:+(y::Parameter, x::Number) = Base.:+(x, y)
 Base.:+(x::Parameter, y::Parameter) = x.ref + y.ref
 
+Base.:-(y::Parameter) = -y.ref
 Base.:-(x::Number, y::Parameter) = x - y.ref
 Base.:-(y::Parameter, x::Number) = y.ref - x
 Base.:-(x::Parameter, y::Parameter) = x.ref - y.ref
 
-Base.:^(x::Number, y::Parameter) = Base.power_by_squaring(x, y.ref)
-Base.:^(y::Parameter, x::Number) = Base.power_by_squaring(y.ref, x)
-Base.:^(x::Parameter, y::Parameter) = Base.power_by_squaring(x.ref, y.ref)
+Base.:^(x::Number, y::Parameter) = Base.:^(x, y.ref)
+Base.:^(y::Parameter, x::Number) = Base.:^(y.ref, x)
+Base.:^(x::Parameter, y::Parameter) = Base.:^(x.ref, y.ref)
 
 Base.isless(x::Parameter, y::Number) = Base.isless(x.ref, y)
 Base.isless(y::Number, x::Parameter) = Base.isless(y, x.ref)
 
 Base.copy(x::Parameter{T}) where {T} = Parameter{T}(copy(x.data), x.ref)
+
+Base.max(x::Number, y::Parameter) = max(x, y.ref)
+Base.max(x::Parameter, y::Number) = max(x.ref, y)
+Base.max(x::Parameter, y::Parameter) = max(x.ref, y.ref)
+
+Base.min(x::Number, y::Parameter) = min(x, y.ref)
+Base.min(x::Parameter, y::Number) = min(x.ref, y)
+Base.min(x::Parameter, y::Parameter) = min(x.ref, y.ref)
+
 
 function Base.show(io::IO, m::MIME"text/plain", p::Parameter)
     if !isempty(p.data)
