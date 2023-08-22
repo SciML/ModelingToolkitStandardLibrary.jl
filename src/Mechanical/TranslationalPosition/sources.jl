4526995266bbe713129c82cf9055e01aa4c68576
Yingbo Ma <mayingbo5@gmail.com>
2023-08-22 14:29:31 -0400

    Update tests and fix use_support support

--- src/Mechanical/TranslationalPosition/sources.jl
+++ src/Mechanical/TranslationalPosition/sources.jl
@@ -5,8 +5,12 @@ Input signal acting as external force on a flange
 """
 @mtkmodel Force begin
     @parameters begin
+        use_support = false
         s = 0
     end
+    @extend (flange,) = partial_element = PartialElementaryOneFlangeAndSupport2(;
+        use_support = use_support isa Bool ? use_support :
+                      ModelingToolkit.getdefault(use_support))
     @components begin
         flange = Flange(; s = s)
         f = RealInput()
