c8cc29d1b397c73e230a1516973e844c4da35dd6
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2023-09-24 09:43:24 +0300

    Add free body test case

--- test/Mechanical/planar_mechanics.jl
+++ test/Mechanical/planar_mechanics.jl
@@ -1,8 +1,30 @@
 using ModelingToolkit, OrdinaryDiffEq, Test
 using ModelingToolkitStandardLibrary.Mechanical.PlanarMechanics
+# using Plots
 
 @parameters t
 D = Differential(t)
+tspan = (0.0, 3.0)
+
+@testset "Free body" begin
+    @named body = Body(m = 1, j = 0.1)
+    @named model = ODESystem(Equation[],
+        t,
+        [],
+        [],
+        systems = [body])
+    sys = structural_simplify(model)
+    unset_vars = setdiff(states(sys), keys(ModelingToolkit.defaults(sys)))
+    prob = ODEProblem(sys, unset_vars .=> 0.0, tspan, []; jac = true)
+
+    sol = solve(prob, Rodas5P())
+    @test SciMLBase.successful_retcode(sol)
+
+    free_falling_displacement = -0.5 * 9.807 * tspan[2]^2  # 0.5 * g * t^2
+    @test sol[body.ry][end] ≈ free_falling_displacement
+    @test sol[body.rx][end] == 0  # no horizontal displacement
+    # plot(sol, idxs = [body.rx, body.ry])
+end
 
 @testset "Pendulum" begin
     @named ceiling = Fixed()
@@ -23,7 +45,7 @@ D = Differential(t)
         systems = [body, revolute, rod, ceiling])
     sys = structural_simplify(model)
     unset_vars = setdiff(states(sys), keys(ModelingToolkit.defaults(sys)))
-    prob = ODEProblem(sys, unset_vars .=> 0.0, (0.0, 60), []; jac = true)
+    prob = ODEProblem(sys, unset_vars .=> 0.0, tspan, []; jac = true)
     sol = solve(prob, Rodas5P())
 
     # phi and omega for the pendulum body
