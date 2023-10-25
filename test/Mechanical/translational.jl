adc4e60faf5777f13670340f18ced63514c7fb94
Brad Carman <brad_carman@instron.com>
2023-10-25 06:00:54 -0400

    fixes free test

--- test/Mechanical/translational.jl
+++ test/Mechanical/translational.jl
@@ -10,7 +10,7 @@ D = Differential(t)
 @testset "Free" begin
     function System(; name)
         systems = @named begin
-            acc = TV.Acceleration()
+            acc = TV.Acceleration(false)
             a = Constant(; k = -10)
             mass = TV.Mass(; m = 100)
             free = TV.Free()
