91840f48acd9a559b3e62630400280c558b7cd17
Venkateshprasad <32921645+ven-k@users.noreply.github.com>
2024-03-10 16:29:33 +0530

    refactor(Rotational): re-enable and refactor first example test

--- test/Mechanical/rotational.jl
+++ test/Mechanical/rotational.jl
@@ -37,7 +37,8 @@ using OrdinaryDiffEq: ReturnCode.Success
     sol = solve(prob, Rodas4())
     @test SciMLBase.successful_retcode(sol)
 
-    prob = DAEProblem(sys, D.(unknowns(sys)) .=> 0.0, [D(D(sys.inertia2.phi)) => 0.0], (0, 10.0))
+    prob = DAEProblem(
+        sys, D.(unknowns(sys)) .=> 0.0, [D(D(sys.inertia2.phi)) => 0.0], (0, 10.0))
     sol = solve(prob, DFBDF())
     @test SciMLBase.successful_retcode(sol)
     @test all(sol[sys.inertia1.w] .== 0)
@@ -99,7 +100,8 @@ end
     sol = solve(prob, DFBDF())
     @test SciMLBase.successful_retcode(sol)
 
-    prob = ODEProblem(sys, [D(D(sys.inertia1.phi)) => 1.0, sys.spring.flange_b.phi => 0.0], (0, 1.0))
+    prob = ODEProblem(
+        sys, [D(D(sys.inertia1.phi)) => 1.0, sys.spring.flange_b.phi => 0.0], (0, 1.0))
     sol = solve(prob, Rodas4())
     @test SciMLBase.successful_retcode(sol)
 
@@ -126,7 +128,8 @@ end
 
     @mtkbuild sys = TwoInertiasWitConstantTorque()
 
-    prob = ODEProblem(sys, [D(D(sys.inertia2.phi)) => 1.0, sys.spring.flange_b.phi => 0.0], (0, 10.0))
+    prob = ODEProblem(
+        sys, [D(D(sys.inertia2.phi)) => 1.0, sys.spring.flange_b.phi => 0.0], (0, 10.0))
     sol = solve(prob, Rodas4())
     @test SciMLBase.successful_retcode(sol)
     @test sol(sol.t[end], idxs = sys.inertia1.w)≈sol(sol.t[end], idxs = sys.inertia2.w) rtol=0.1 # both inertias have same angular velocity after initial transient
@@ -134,52 +137,46 @@ end
 
 # see: https://doc.modelica.org/Modelica%204.0.0/Resources/helpWSM/Modelica/Modelica.Mechanics.Rotational.Examples.First.html
 @testset "first example" begin
-    amplitude = 10 # Amplitude of driving torque
-    frequency = 5 # Frequency of driving torque
-    J_motor = 0.1 # Motor inertia
-    J_load = 2 # Load inertia
-    ratio = 10 # Gear ratio
-    damping = 10 # Damping in bearing of gear
+    @mtkmodel FirstExample begin
+        @structural_parameters begin
+            amplitude = 10 # Amplitude of driving torque
+            frequency = 5 # Frequency of driving torque
+            J_motor = 0.1 # Motor inertia
+            J_load = 2 # Load inertia
+            ratio = 10 # Gear ratio
+            damping = 10 # Damping in bearing of gear
+        end
 
-    @named fixed = Fixed()
-    @named torque = Torque(use_support = true)
-    @named inertia1 = Inertia(J = J_motor)
-    @named idealGear = IdealGear(ratio = ratio, use_support = true)
-    @named inertia2 = Inertia(J = 2)
-    @named spring = Spring(c = 1e4)
-    @named inertia3 = Inertia(J = J_load)
-    @named damper = Damper(d = damping)
-    @named sine = Blocks.Sine(amplitude = amplitude, frequency = frequency)
+        @components begin
+            fixed = Fixed()
+            torque = Torque(use_support = true)
+            inertia1 = Inertia(J = J_motor)
+            idealGear = IdealGear(ratio = ratio, use_support = true)
+            inertia2 = Inertia(J = 2)
+            spring = Spring(c = 1e4)
+            inertia3 = Inertia(J = J_load)
+            damper = Damper(d = damping)
+            sine = Blocks.Sine(amplitude = amplitude, frequency = frequency)
+        end
 
-    connections = [connect(inertia1.flange_b, idealGear.flange_a)
-                   connect(idealGear.flange_b, inertia2.flange_a)
-                   connect(inertia2.flange_b, spring.flange_a)
-                   connect(spring.flange_b, inertia3.flange_a)
-                   connect(damper.flange_a, inertia2.flange_b)
-                   connect(damper.flange_b, fixed.flange)
-                   connect(sine.output, torque.tau)
-                   connect(torque.support, fixed.flange)
-                   connect(idealGear.support, fixed.flange)
-                   connect(torque.flange, inertia1.flange_a)]
-
-    @named model = ODESystem(connections, t,
-        systems = [
-            fixed,
-            torque,
-            inertia1,
-            idealGear,
-            inertia2,
-            spring,
-            inertia3,
-            damper,
-            sine
-        ])
-    @test_skip begin
-        sys = structural_simplify(model) #key 7 not found
-        prob = ODEProblem(sys, Pair[], (0, 1.0))
-        sol = solve(prob, Rodas4())
-        @test SciMLBase.successful_retcode(sol)
+        @equations begin
+            connect(inertia1.flange_b, idealGear.flange_a)
+            connect(idealGear.flange_b, inertia2.flange_a)
+            connect(inertia2.flange_b, spring.flange_a)
+            connect(spring.flange_b, inertia3.flange_a)
+            connect(damper.flange_a, inertia2.flange_b)
+            connect(damper.flange_b, fixed.flange)
+            connect(sine.output, torque.tau)
+            connect(torque.support, fixed.flange)
+            connect(idealGear.support, fixed.flange)
+            connect(torque.flange, inertia1.flange_a)
+        end
     end
+
+    sys = structural_simplify(model) #key 7 not found
+    prob = ODEProblem(sys, [inertia3.w => 0.0, spring.flange_a.phi => 0.0], (0, 1.0))
+    sol = solve(prob, Rodas4())
+    @test SciMLBase.successful_retcode(sol)
     # Plots.plot(sol; vars=[inertia2.w, inertia3.w])
 end
 
@@ -205,7 +202,7 @@ end
             damper = Damper(d = 0.01)
             inertia = Inertia(J = 0.0001)
             friction = RotationalFriction(f = 0.001, tau_c = 20, w_brk = 0.06035,
-            tau_brk = 25)
+                tau_brk = 25)
             vel_profile = VelocityProfile()
             source = Speed()
             angle_sensor = AngleSensor()
@@ -222,7 +219,8 @@ end
     end
 
     @mtkbuild sys = StickSlip()
-    prob = DAEProblem(sys, D.(unknowns(sys)) .=> 0.0, [sys.inertia.flange_b.tau => 0.0; unknowns(sys) .=> 0.0...], (0, 10.0))
+    prob = DAEProblem(sys, D.(unknowns(sys)) .=> 0.0,
+        [sys.inertia.flange_b.tau => 0.0; unknowns(sys) .=> 0.0...], (0, 10.0))
 
     sol = solve(prob, DFBDF())
     @test SciMLBase.successful_retcode(sol)
@@ -237,7 +235,6 @@ end
 end
 
 @testset "sensors" begin
-
     @mtkmodel Sensors begin
         @components begin
             fixed = Fixed()
@@ -270,7 +267,8 @@ end
     @test all(sol[sys.rel_speed_sensor.w_rel.u] .== sol[sys.speed_sensor.w.u])
     @test all(sol[sys.torque_sensor.tau.u] .== -sol[sys.inertia1.flange_b.tau])
 
-    prob = DAEProblem(sys, D.(unknowns(sys)) .=> 0.0, [D(D(sys.inertia2.phi)) => 0.0], (0, 10.0))
+    prob = DAEProblem(
+        sys, D.(unknowns(sys)) .=> 0.0, [D(D(sys.inertia2.phi)) => 0.0], (0, 10.0))
     sol = solve(prob, DFBDF())
     @test SciMLBase.successful_retcode(sol)
     @test all(sol[sys.inertia1.w] .== 0)
