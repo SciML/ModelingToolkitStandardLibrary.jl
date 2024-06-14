12ea8f851fd2dcafaae5a295adc39872f50795ca
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-06-14 06:32:57 -0400

    Fix testing setup

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,69 +1,75 @@
 using SafeTestsets
 
-@safetestset "Quality Assurance" begin
-    include("qa.jl")
-end
+@time begin
+    if GROUP == "QA" || GROUP == "All"
+        @time @safetestset "Aqua" begin
+            include("aqua.jl")
+        end
+    end
 
-# Blocks
-@safetestset "Blocks: utils" begin
-    include("Blocks/utils.jl")
-end
-@safetestset "Blocks: math" begin
-    include("Blocks/math.jl")
-end
-@safetestset "Blocks: nonlinear" begin
-    include("Blocks/nonlinear.jl")
-end
-@safetestset "Blocks: continuous" begin
-    include("Blocks/continuous.jl")
-end
-@safetestset "Blocks: sources" begin
-    include("Blocks/sources.jl")
-end
-@safetestset "Blocks: analysis points" begin
-    include("Blocks/test_analysis_points.jl")
-end
+    if GROUP == "Core" || GROUP == "All"
+        # Blocks
+        @safetestset "Blocks: utils" begin
+            include("Blocks/utils.jl")
+        end
+        @safetestset "Blocks: math" begin
+            include("Blocks/math.jl")
+        end
+        @safetestset "Blocks: nonlinear" begin
+            include("Blocks/nonlinear.jl")
+        end
+        @safetestset "Blocks: continuous" begin
+            include("Blocks/continuous.jl")
+        end
+        @safetestset "Blocks: sources" begin
+            include("Blocks/sources.jl")
+        end
+        @safetestset "Blocks: analysis points" begin
+            include("Blocks/test_analysis_points.jl")
+        end
 
-# Electrical
-@safetestset "Analog Circuits" begin
-    include("Electrical/analog.jl")
-end
+        # Electrical
+        @safetestset "Analog Circuits" begin
+            include("Electrical/analog.jl")
+        end
 
-@safetestset "Digital Circuits" begin
-    include("Electrical/digital.jl")
-end
-@safetestset "Chua Circuit Demo" begin
-    include("chua_circuit.jl")
-end
+        @safetestset "Digital Circuits" begin
+            include("Electrical/digital.jl")
+        end
+        @safetestset "Chua Circuit Demo" begin
+            include("chua_circuit.jl")
+        end
 
-# Thermal
-@safetestset "Thermal Circuits" begin
-    include("Thermal/thermal.jl")
-end
-@safetestset "Thermal Demo" begin
-    include("Thermal/demo.jl")
-end
+        # Thermal
+        @safetestset "Thermal Circuits" begin
+            include("Thermal/thermal.jl")
+        end
+        @safetestset "Thermal Demo" begin
+            include("Thermal/demo.jl")
+        end
 
-# Magnetic
-@safetestset "Magnetic" begin
-    include("Magnetic/magnetic.jl")
-end
+        # Magnetic
+        @safetestset "Magnetic" begin
+            include("Magnetic/magnetic.jl")
+        end
 
-# Mechanical
-@safetestset "Mechanical Rotation" begin
-    include("Mechanical/rotational.jl")
-end
-@safetestset "Mechanical Translation" begin
-    include("Mechanical/translational.jl")
-end
-@safetestset "Mechanical Translation" begin
-    include("Mechanical/translational_modelica.jl")
-end
-@safetestset "Multi-Domain" begin
-    include("multi_domain.jl")
-end
+        # Mechanical
+        @safetestset "Mechanical Rotation" begin
+            include("Mechanical/rotational.jl")
+        end
+        @safetestset "Mechanical Translation" begin
+            include("Mechanical/translational.jl")
+        end
+        @safetestset "Mechanical Translation" begin
+            include("Mechanical/translational_modelica.jl")
+        end
+        @safetestset "Multi-Domain" begin
+            include("multi_domain.jl")
+        end
 
-# Hydraulic
-@safetestset "Hydraulic IsothermalCompressible" begin
-    include("Hydraulic/isothermal_compressible.jl")
+        # Hydraulic
+        @safetestset "Hydraulic IsothermalCompressible" begin
+            include("Hydraulic/isothermal_compressible.jl")
+        end
+    end
 end
