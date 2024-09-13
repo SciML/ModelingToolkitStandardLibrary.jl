d7b85edf26fb5101ad6f0034a04a66ad817c353a
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2024-09-13 08:01:31 +0200

    add docstring

--- src/Mechanical/TranslationalModelica/sources.jl
+++ src/Mechanical/TranslationalModelica/sources.jl
@@ -14,6 +14,18 @@ Input signal acting as external force on a flange
     end
 end
 
+"""
+    Position(; name, exact = false, f_crit = 50)
+
+Forced movement of a flange according to a reference position
+
+The input signal `s_ref` defines the reference position in [m]. Flange flange is forced to move relative to the support connector according to this reference motion. According to parameter `exact`, this is done in the following way:
+
+- `exact=true`: The reference position is treated exactly. This is only possible, if the input signal is defined by an analytical function which can be differentiated at least twice. If this prerequisite is fulfilled, the Modelica translator will differentiate the input signal twice in order to compute the reference acceleration of the flange.
+- `exact=false`: The reference position is filtered and the second derivative of the filtered curve is used to compute the reference acceleration of the flange. This second derivative is not computed by numerical differentiation but by an appropriate realization of the filter. For filtering, a second order Bessel filter is used. The critical frequency (also called cut-off frequency) of the filter is defined via parameter `f_crit` in [Hz]. This value should be selected in such a way that it is higher as the essential low frequencies in the signal.
+
+The input signal can be provided from one of the signal generator blocks of the block library `Blocks.Sources`.
+"""
 @mtkmodel Position begin
     @extend (s,) = ptf = PartialElementaryOneFlangeAndSupport2()
     @structural_parameters begin
